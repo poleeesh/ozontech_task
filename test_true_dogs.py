691459bd608dc5551440029ac795e205e1a8a15a
poleeesh <90454726+poleeesh@users.noreply.github.com>
2024-10-11 17:41:41 +0300

    Update test_true_dogs.py

--- test_true_dogs.py
+++ test_true_dogs.py
@@ -1,13 +1,13 @@
 import random
+from time import sleep
 
 import allure
 import pytest
 import requests
 
-
 YANDEX_DISK_URL = 'https://cloud-api.yandex.net/v1/disk/resources'
 DOG_CEO_URL = 'https://dog.ceo/api/breed/'
-TOKEN = 'AgAAAAAJtest_tokenxkUEdew'
+TOKEN = 'y0_AgAAAABXEqcBAADLWwAAAAEUEQJYAAA25CY0aFpAmYBDwMJeAoggT0Vvog'
 FOLDER_NAME = 'test_folder'
 HEADERS = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
 
@@ -26,10 +26,14 @@ class YaUploader:
         requests.post(url, headers=HEADERS, params=params)
 
     @staticmethod
-    def delete_photos_from_yd(path: str, url_file: str, name: str) -> None:
-        url = YANDEX_DISK_URL + "/delete"
-        params = {"path": f'/{path}/{name}', 'url': url_file, "overwrite": "true"}
-        requests.post(url, headers=HEADERS, params=params)
+    def delete_photos_from_yd(path: str, name: str) -> None:
+        params = {"path": f'/{path}/{name}', "permanently": "true"}
+        requests.delete(YANDEX_DISK_URL, headers=HEADERS, params=params)
+
+    @staticmethod
+    def update_photos(path: str):
+        params = {"path": f'/{path}/', "body": "{'custom_properties': {}}"}
+        requests.patch(YANDEX_DISK_URL, headers=HEADERS, params=params)
 
 
 class DogCeoApi:
@@ -66,13 +70,17 @@ class TestDogUpload:
                 part_name = url.split('/')[-2:]
                 name = '_'.join(part_name)
                 YaUploader.upload_photos_to_yd(FOLDER_NAME, url, name)
+            YaUploader.update_photos(FOLDER_NAME)
+            # даже после обновления ресурса методом /patch тест иногда не получал текущую информацию
+            # так что пришлось добавить таймаут ¯\_(ツ)_/¯
+            sleep(10)
         yield
         with allure.step("Delete photos"):
             for url in urls:
                 part_name = url.split('/')[-2:]
                 name = '_'.join(part_name)
-                YaUploader.delete_photos_from_yd(FOLDER_NAME, url, name)
-        
+                YaUploader.delete_photos_from_yd(FOLDER_NAME, name)
+
     @pytest.mark.parametrize('breed', TEST_BREEDS)
     @pytest.mark.usefixtures("upload_photos")
     @allure.title("Проверка программы для загрузки картинок собак")
@@ -84,10 +92,11 @@ class TestDogUpload:
 
         with allure.step("Assert info about files in response"):
             response_items = response['_embedded']['items']
-            if not DogCeoApi.get_sub_breeds(breed):
+            sub_breeds = DogCeoApi.get_sub_breeds(breed)
+            if not sub_breeds:
                 assert len(response_items) == 1
             else:
-                assert len(response_items) == len(DogCeoApi.get_sub_breeds(breed))
+                assert len(response_items) == len(sub_breeds)
 
             for item in response_items:
                 assert item['type'] == 'file'
